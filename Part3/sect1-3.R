310679aba07140d8f8f108214d30e90d5d8a9274
Leejinsam <kist9620@naver.com>
2019-05-29 15:11:56 +0900

    Part3, Section 1-3

---
+++ Part3/sect1-3.R
@@ -0,0 +1,21 @@
+setwd("d:/workspace/R_Data_Analysis/Part3/data")
+getwd()
+
+
+print(1+2)
+1+2
+print('a')
+"a"
+'string'
+print(pi)
+print(pi, digits=3)
+
+print(3,4) # 하나만 찍힌다
+#코딩 주의할점 두가지
+# 1) Indentation (조건식,반복문,함수) 
+# 2) Comment 
+
+print('a', 'b') #에러발생 
+cat(1,':','a','\n',2,':','b')
+3;4;5;
+1+2 ; 3*4; pi*5*5
